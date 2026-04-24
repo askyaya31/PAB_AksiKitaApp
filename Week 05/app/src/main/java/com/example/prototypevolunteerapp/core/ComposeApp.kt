693619ad7bfc39c085b89d64b269611307d8336c
askyaya31 <saskyaaliyaazizah06@gmail.com>
2026-04-24 22:34:23 +0700

    Refactor navigation entries and add volunteers logic

--- Week 05/app/src/main/java/com/example/prototypevolunteerapp/core/ComposeApp.kt
+++ Week 05/app/src/main/java/com/example/prototypevolunteerapp/core/ComposeApp.kt
@@ -2,11 +2,14 @@ package com.example.prototypevolunteerapp.core
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
+import com.example.prototypevolunteerapp.data.model.getDummyVolunteers
+import com.example.prototypevolunteerapp.ui.screens.LoginScreen
 import com.example.prototypevolunteerapp.ui.screens.activities.ActivitiesScreen
 import com.example.prototypevolunteerapp.ui.screens.activities.ActivityDetailScreen
 import com.example.prototypevolunteerapp.ui.screens.dashboard.HomeScreen
@@ -19,55 +22,38 @@ import com.example.prototypevolunteerapp.ui.theme.AksiKitaTheme
 
 @Composable
 fun ComposeApp() {
-    val backStack = rememberNavBackStack(Routes.WelcomeRoute)
+    val backStack  = rememberNavBackStack(Routes.WelcomeRoute)
+    val volunteers = remember { getDummyVolunteers() }
     CompositionLocalProvider(LocalBackStack provides backStack) {
         AksiKitaTheme {
             NavDisplay(
                 backStack = backStack,
                 entryDecorators = listOf(
-                    // Menyimpan state composable saat navigasi
                     rememberSaveableStateHolderNavEntryDecorator(),
-                    // Mengelola ViewModel per layar
                     rememberViewModelStoreNavEntryDecorator()
                 ),
                 entryProvider = entryProvider {
-                    entry<Routes.WelcomeRoute> {WelcomeScreen()}
-                    entry<Routes.HomeRoute> {HomeScreen(userName = it.userName)}
-                    entry<Routes.ActivitiesRoute> {
-                        ActivitiesScreen()
-                    }
-                    //it buat objek ActivityDetailRoute isinya berisi semua properti
+                    entry<Routes.WelcomeRoute> { WelcomeScreen() }
+                    entry<Routes.LoginRoute> { LoginScreen() }
+                    entry<Routes.HomeRoute> { HomeScreen() }
+                    entry<Routes.ActivitiesRoute> { ActivitiesScreen() }
                     entry<Routes.ActivityDetailRoute> {
                         ActivityDetailScreen(
-                            title    = it.title,
-                            location = it.location,
-                            desc     = it.desc,
-                            imageRes = it.imageRes,
-                            instagram = it.instagram.ifBlank {null },
-                            link     = it.link.ifBlank {null }
+                            title  =it.title,
+                            location  =it.location,
+                            desc      =it.desc,
+                            imageRes  =it.imageRes,
+                            instagram =it.instagram.ifBlank { null },
+                            link      =it.link.ifBlank { null }
                         )
                     }
-
-                    entry<Routes.VolunteersRoute> {
-                        VolunteersScreen()
-                    }
-
+                    entry<Routes.VolunteersRoute> { VolunteersScreen() }
                     entry<Routes.VolunteerDetailRoute> {
-                        VolunteerDetailScreen(
-                            name     = it.name,
-                            desc     = it.desc,
-                            imageRes = it.imageRes,
-                            phone    = it.phone.ifBlank { null },
-                            email    = it.email.ifBlank { null }
-                        )
+                        val volunteer = volunteers.getOrNull(it.volunteerIndex)
+                        if (volunteer != null) {VolunteerDetailScreen(volunteer = volunteer)}
                     }
-
-                    entry<Routes.ActivityHistoryRoute> {
-                        ActivityHistoryScreen()
-                    }
-                    entry<Routes.NotificationsRoute> {
-                        NotificationsScreen()
-                    }
+                    entry<Routes.ActivityHistoryRoute> { ActivityHistoryScreen() }
+                    entry<Routes.NotificationsRoute> { NotificationsScreen() }
                 }
             )
         }
